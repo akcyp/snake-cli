5ce3607b4e842d7e4f1f572ec494585dfff853da
akcyp <pycka.bartosz@gmail.com>
2020-07-31 22:27:08 +0200

    Module usage support

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@
 
 import SnakeGame from './SnakeGame';
 
-const game = new SnakeGame({
-  moveThroughWall: false,
-  speed: 10,
-}).on('gameOver', () => {
-  process.stdin.on('keypress', () => process.exit());
-}).init();
+if (require.main === module) {
+  new SnakeGame({
+    moveThroughWall: false,
+    speed: 10,
+  }).on('gameOver', () => {
+    process.stdin.on('keypress', () => process.exit());
+  }).init();
+}
 
-
+export default SnakeGame;
